74a6d26084d7fc2e57e53995aa5737b7969b6ac3
Himanshu Mahtolia <114689084+himanshumahtolia@users.noreply.github.com>
2023-08-28 22:49:05 +0530

    Update Week_1__1_Linear_Search.cpp

--- Week_1__1_Linear_Search.cpp
+++ Week_1__1_Linear_Search.cpp
@@ -1,10 +1,6 @@
   #include<bits/stdc++.h>
   using namespace std;
-  
-  // working function..........
-  
    void search_linear(int arr[],int n,int key){
-   	   // for linear search i will check one by one element...
    	  int flag=0,c=0,i=0;
    	  while(i<n){
         c++;
